25c7b62a3b30bc0813aec1e8802273da4f653785
LuisAndagoya <manaeneldesierto@hotmail.com>
2019-01-13 16:34:06 -0500

    reservas

---
+++ SiHotelFV/CapaProceso/Clases/ModeloReservacionDetalle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaProceso.Clases
+{
+    public class ModeloReservacionDetalle
+    {
+        private int idDetalle { get; set; }
+        private int idReserva { get; set; }
+        private int numeroHabitacion { get; set; }
+        private string fechaActual { get; set; }
+        private float valor { get; set; }
+        public void load(int idDetalle, int idReserva, int numeroHabitacion, string fechaActual, float valor)
+        {
+            this.idDetalle = idDetalle;
+            this.idReserva = idReserva;
+            this.numeroHabitacion = numeroHabitacion;
+            this.fechaActual = fechaActual;
+            this.valor = valor;
+
+        }
+
+       
+
+
+    }
+}
